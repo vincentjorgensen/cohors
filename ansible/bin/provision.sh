d45dce968eb745ba40e94e981d5c2409187885ac
Vincent Jorgensen <vincent.jorgensen@gmail.com>
2016-05-26 15:25:00 -0700

    Better usage, -c|-d flags

--- ansible/bin/provision.sh
+++ ansible/bin/provision.sh
@@ -7,22 +7,36 @@ cf_dir=$base_dir/cf
 
 function usage {
     cat <<EOF
-Usage: $0 [-s <stack>] [-r <region>] [-e <environs>] 
+Usage:
+$0 [-c|-d] [-s <stack>] [-r <region>] [-e <environs>] 
     or
-     export CH_REGION=us-west-2
-     export CH_ENVIRONS=dev
-     $0 [stack]
+export CH_REGION=us-west-2
+export CH_ENVIRONS=dev
+export CH_STATE=present    # absent or -d to remove
+$0 [stack]
+
+The flags will override the environment variables
 EOF
     exit
 }
 
-region=$CH_REGION
-environs=$CH_ENVIRONS
+region=us-west-2
+environs=dev
+stack_state=present
+[[ -n $CH_REGION ]]   && region=$CH_REGION
+[[ -n $CH_ENVIRONS ]] && environs=$CH_ENVIRONS
+[[ -n $CH_STATE ]]    && stack_state=$CH_ENVIRONS
 stack=$1
 [[ -z $2 ]] && shift
 
-while getopts ":s:r:e:" opt; do
+while getopts ":s:r:e:cd" opt; do
     case $opt in
+        c)
+            stack_state=present
+            ;;
+        d)
+            stack_state=absent
+            ;;
         s)
             stack=$OPTARG
             ;;
@@ -51,6 +65,8 @@ ch_file=$base_dir/stacks/$stack.yml
 cf_file=$base_dir/cf/$region-$stack-$environs.cftemplate
 $base_dir/bin/y2j.py < $ch_file > $cf_file
 
-ansible-playbook -vvvvv -i $base_dir/hosts --extra-vars "stack=$stack region=$region environs=$environs base_dir=$base_dir" $playbook $*
+ansible-playbook -vvvvv -i $base_dir/hosts \
+    --extra-vars "stack=$stack region=$region environs=$environs base_dir=$base_dir stack_state=$stack_state" \
+    $playbook $*
 
 # End

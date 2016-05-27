b5ec6d5a620e775f2bdbbbf925e3d3ff3e4aa8fa
Vincent Jorgensen <vincent.jorgensen@gmail.com>
2016-05-26 18:29:51 -0700

    Making it more Latin

---
+++ ansible/bin/cohors.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+base_dir=$DIR/..
+provision_dir=$base_dir/centariae
+cf_dir=$base_dir/cf
+
+cmd=${0##*/}
+
+function usage {
+    cat <<EOF
+Usage:
+`readlink $0` [-c|-d] [-s <stack>] [-r <region>] [-e <environs>] 
+    or
+export CH_REGION=us-west-2
+export CH_ENVIRONS=dev
+export CH_STATE=present    # absent or -d to remove
+`readlink $0` [stack]
+
+The flags will override the environment variables
+EOF
+    exit
+}
+
+region=us-west-2
+environs=dev
+stack_state=present
+[[ -n $CH_REGION ]]     && region=$CH_REGION
+[[ -n $CH_ENVIRONS ]]   && environs=$CH_ENVIRONS
+[[ -n $CH_STATE ]]      && stack_state=$CH_ENVIRONS
+[[ $cmd = procedite ]]  && stack_state=present
+[[ $cmd = consistite ]] && stack_state=absent
+stack=$1
+[[ -z $2 ]] && shift
+
+while getopts ":s:r:e:cd" opt; do
+    case $opt in
+        c)
+            stack_state=present
+            ;;
+        d)
+            stack_state=absent
+            ;;
+        s)
+            stack=$OPTARG
+            ;;
+        r)
+            region=$OPTARG
+            ;;
+        e)
+            environs=$OPTARG
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+playbook=$provision_dir/$stack.yml
+
+[[ -z $region ]] && usage
+[[ -z $stack ]] && usage
+[[ -z $environs ]] && usage
+[[ ! -e $playbook ]] && echo "$playbook must exist" && exit 1
+
+ch_file=$base_dir/stacks/$stack.yml
+cf_file=$base_dir/cf/$region-$stack-$environs.cftemplate
+$base_dir/bin/y2j.py < $ch_file > $cf_file
+
+ansible-playbook -vvvvv -i $base_dir/hosts \
+    --extra-vars "stack=$stack region=$region environs=$environs base_dir=$base_dir stack_state=$stack_state" \
+    $playbook $*
+
+# End

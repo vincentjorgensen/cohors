363d043c9356fc947d5aa9c8bcea165fc1d3f3aa
Vincent Jorgensen <vincent.jorgensen@gmail.com>
2016-06-29 17:13:56 -0700

    Added back in lost full refresh details

--- ansible/bin/refresh_amis.sh
+++ ansible/bin/refresh_amis.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-regions=(us_west_2 us_west_1 us_east_1 eu_west_1 eu_central_1)
+regions=(us_west_2 us_west_1 us_east_1 eu_west_1 eu_central_1 ap_southeast_1 ap_southeast_2 ap_northeast_1 ap_northeast_2 sa_east_1)
 
 trusty=trusty-14.04
 xenial=xenial-16.04
-tmp=$(mktemp)
+#tmp=$(mktemp)
 
 cat <<EOF
 ---
@@ -21,24 +21,31 @@ function aws_describe_images {
         "Name=is-public,Values=true" "Name=owner-id,Values=099720109477"
 }
 
+function get_latest {
+    l_region=$1
+    l_image=$2
+    aws_describe_images $l_region $l_image '*' |\
+        egrep "\<Name\>"|awk -F'-' '{print $NF}'|sed -e 's/[","]//g'|sort -rn|head -n 1
+}
+
 # latest Xenial
-aws_describe_images us-west-2 $xenial '*' |\
-    egrep "\<Name\>"|awk -F'-' '{print $NF}'|sed -e 's/[","]//g'|sort -rn|head -n 1 \
-    > $tmp
-xlatest=`cat $tmp`
+#aws_describe_images us-west-2 $xenial '*' |\
+#    egrep "\<Name\>"|awk -F'-' '{print $NF}'|sed -e 's/[","]//g'|sort -rn|head -n 1 \
+#    > $tmp
+#xlatest=`cat $tmp`
 
 # latest Trusty
-aws_describe_images us-west-2 $trusty '*' |\
-    egrep "\<Name\>"|awk -F'-' '{print $NF}'|sed -e 's/[","]//g'|sort -rn|head -n 1 \
-    > $tmp
-tlatest=`cat $tmp`
+#aws_describe_images us-west-2 $trusty '*' |\
+#    egrep "\<Name\>"|awk -F'-' '{print $NF}'|sed -e 's/[","]//g'|sort -rn|head -n 1 \
+#    > $tmp
+#tlatest=`cat $tmp`
 
 # Xenial
 echo "  xenial:"
 for region in ${regions[*]}; do
     r=`echo $region|sed -e 's/_/-/g'`
     echo -n "    $region: "
-    aws_describe_images $r $xenial $xlatest |\
+    aws_describe_images $r $xenial `get_latest $r $xenial` |\
         grep ImageId | awk -F: '{print $2}'|sed -e 's/[",]//g'|head -n 1
 done
 
@@ -47,6 +54,7 @@ echo "  trusty:"
 for region in ${regions[*]}; do
     r=`echo $region|sed -e 's/_/-/g'`
     echo -n "    $region: "
-    aws_describe_images $r $trusty $tlatest |\
+    aws_describe_images $r $trusty `get_latest $r $trusty` |\
         grep ImageId | awk -F: '{print $2}'|sed -e 's/[",]//g'|head -n 1
 done
+
